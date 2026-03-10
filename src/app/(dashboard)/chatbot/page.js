f601f6c76a2df62732857720ccef2258bc4ec106
Ahmedfci378 <ahmedfci378@gmail.com>
2026-03-10 16:42:25 +0200

    updated

--- src/app/(dashboard)/chatbot/page.js
+++ src/app/(dashboard)/chatbot/page.js
@@ -16,7 +16,7 @@ export default function ChatbotPage() {
      اتصال Socket
   ==========================*/
   useEffect(() => {
-    const socket = io("http://localhost:3000", {
+    const socket = io(process.env.NEXT_PUBLIC_API_BASE, {
       query: { sessionId: sessionIdRef.current },
       transports: ["websocket"],
     });
